ea5eb6f295b7f84ed4838523ec703232daefb656
Thomas Jespersen <laumann.thomas@gmail.com>
2012-10-17 09:59:42 +0200

    Init files on FUN from the course SaT.

---
+++ FUN.hs
@@ -0,0 +1,118 @@
+{-
+  This is an implementation of the FUN language as presented in the 
+  DIKU course 'Semantics and Types' (SaT).
+-}
+module FUN where
+
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Language (haskellStyle)
+
+lambda = "λ"
+leq    = "≤"
+
+-- Reserved words in FUN
+reserved_words = ["true", "false", "if", "then", "else", "fst", 
+                  "snd", lambda, "let", "in", "rec" ]
+
+type Id = String
+
+-- | Grammar of FUN
+data Term = TNum Integer
+          | TBool Bool
+          | TVar Id
+          | TPlus Term Term
+          | TLeq Term Term
+          | TIf Term Term Term
+          | TPair Term Term
+          | TFst Term
+          | TSnd Term
+          -- | TLam TVar Term
+          -- | TApp Term Term
+          -- | TLet TVar Term Term
+          -- | TRec TVar Term
+          deriving Show
+
+type Program = [Term]
+
+nesting :: Parser Int
+nesting = do char '('
+             n <- nesting
+             char ')'
+             m <- nesting
+             return $ max (n+1) m
+          <|> return 0
+
+--num = token $ do str <- many1 digit
+  --               return $ read str
+
+
+program :: Parser Program
+program = many1 term
+
+
+term :: Parser Term
+term = tNum <|> tBool <|> tVar <|> tIf
+
+tNum = do spaces 
+          n <- num
+          return $ TNum n
+
+tBool = do spaces
+           t <- (symbol "true" <|> symbol "false")
+           return $ TBool $ t == "true"
+
+tVar = do v <- var
+          return $ TVar v
+
+tPlus = undefined
+
+tLeq = undefined
+
+tIf = do keyword "if"
+         t0 <- term
+         keyword "then"
+         t1 <- term
+         keyword "else"
+         t2 <- term
+         return $ TIf t0 t1 t2
+
+tPair = undefined
+
+tFst = undefined
+
+tSnd = undefined
+
+tLam = undefined
+
+tApp = undefined
+
+tLet = undefined
+
+tRec = undefined
+
+num :: Parser Integer
+num = do s <- many1 digit
+         return $ read s
+
+var :: Parser Id
+var = do spaces
+         v <- letter
+         vs <- many (letter <|> digit)
+         return $ v:vs
+         
+
+keyword :: String -> Parser ()         
+keyword s = spaces >> (try $ string s) >> return ()
+
+symbol :: String -> Parser String
+symbol s = try $ string s >>= \s -> return s
+              
+
+run :: (Show a) => Parser a -> String -> IO ()
+run p input = case (parse p "" input) of
+  Right x  -> print x
+  Left err -> do putStr "parse error at "
+                 print err
+
+runEof :: Parser a -> String -> IO ()
+runEof p = run (p >> eof)
---
+++ TypeInfer.hs
@@ -0,0 +1,89 @@
+
+import qualified Data.Map as Map
+import Data.List (intercalate)
+
+data OpenType = OTInt
+              | OTBool
+              | OTProduct OpenType OpenType
+              | OTFunc OpenType OpenType
+              | OTUniVar UnificationVariable
+              deriving (Eq)
+
+data UnificationVariable = UV Integer deriving (Eq,Ord)
+
+instance Show OpenType where
+  show (OTInt)             = "int"
+  show (OTBool)            = "bool"
+  show (OTProduct ot1 ot2) = (show ot1) ++ " × " ++ (show ot2)
+  show (OTFunc ot1 ot2)    = (show ot1) ++ " → " ++ (show ot2)
+  show (OTUniVar uv)       = show uv
+
+instance Show UnificationVariable where
+  show (UV i) = "(" ++ show i ++ ")"
+
+univ :: Integer -> UnificationVariable
+univ i = UV i
+
+otUniv :: Integer -> OpenType -- (OTUniVar (UV i))
+otUniv = OTUniVar . univ
+
+-- A type substitution maps unification variables to (possibly open) types
+type TypeSub = [(UnificationVariable, OpenType)]
+
+showSubst sub = putStrLn $ "θ = [" ++ showSubs sub ++ "]"
+  where showSubs s = intercalate ", " $ [ show uv ++ " → " ++ show ot  | (uv, ot) <- s ]
+
+-- | Reflects the type 
+data TypeConstraint = TC OpenType OpenType
+
+instance Show TypeConstraint where
+  show (TC ot1 ot2) = show ot1 ++ " =*= " ++ show ot2
+
+type TypeEnv = [TypeConstraint]
+
+-- | Substituting open type variables using the given type substitution
+-- There is a more generic way of applying substitutions (rather than just traversing the list)
+-- We must exploit that all the unification variables in the TypeSub are distinct
+applySubst :: OpenType -> TypeSub -> OpenType
+applySubst t [] = t
+applySubst t (ts:tss) = let (i, ot) = ts
+                        in case t of
+                          OTInt -> applySubst t tss
+                          OTBool -> applySubst t tss
+                          (OTProduct ot1 ot2) -> OTProduct (applySubst ot1 (ts:tss)) (applySubst ot2 (ts:tss))
+                          (OTFunc ot1 ot2) -> OTFunc (applySubst ot1 (ts:tss)) (applySubst ot2 (ts:tss))
+                          (OTUniVar j) -> if i == j
+                                          then applySubst ot tss
+                                          else applySubst (OTUniVar j) tss
+
+-- | Does not work as intended...
+applySubstTC :: TypeConstraint -> TypeSub -> TypeConstraint
+applySubstTC (TC ot1 ot2) ts = TC (applySubst ot1 ts) (applySubst ot2 ts)
+
+-- A type environment maps term variables (strings) to open types
+
+-- | UV(t) = the set of unification variables occuring in t
+uV :: OpenType -> [OpenType]
+uV t = undefined
+
+
+-- | Testing
+-- Try out: applySubst tType tSubst1
+--          applySubst tType tSubst2
+tType = OTFunc (otUniv 3) (OTProduct (otUniv 4) OTInt)
+
+tSubst1 :: TypeSub
+tSubst1 = [ (univ 2, OTBool)
+          , (univ 3, (OTUniVar (univ 4)))
+          ]
+
+tSubst2 :: TypeSub
+tSubst2 = [ (univ 2, OTBool)
+          , (univ 3, (OTProduct OTInt OTInt))
+          , (univ 4, (OTProduct OTInt OTInt))
+          ]
+
+tType1 = OTFunc (otUniv 3) OTBool
+tType2 = OTFunc (otUniv 2) (otUniv 4)
+
+tc = TC tType1 tType2
